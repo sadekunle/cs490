e38a3fcbdc06949e3149b17f51a7e4281c19e325
Desmond Johnson <dj65@njit.edu>
2013-10-10 19:41:43 -0400

    edited two days ago, but didn't get a change to commit. So commiting now

--- execute.php
+++ execute.php
@@ -4,8 +4,11 @@ echo '<pre>';
 // Outputs all the result of shellcommand "ls", and returns
 // the last output line into $last_line. Stores the return value
 // of the shell command in $retval.
-$newString =  exec('ls --help');
+//$newString =  exec('java Compiler '.'First Line\nSecond Line');
+$newString =  exec('java /afs/cad/u/d/j/dj65/public_html/cs490/Compiler '.'First Line\nSecond Line');
+
 echo $newString;
+
 /*$last_line = system('ls --help', $retval);*/
 // Printing additional info
 /*echo '
