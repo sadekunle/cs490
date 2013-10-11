8e9e9937765795e6e995c8b1745581ee4e3aad58
Desmond Johnson <dj65@njit.edu>
2013-10-10 23:04:15 -0400

    edited two days ago, but didn't get a change to commit. So commiting now

--- Compiler.java
+++ Compiler.java
@@ -1,16 +1,22 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
 import java.io.PrintWriter;
 
 public class Compiler {
     public static void main(String[] args) {
-		PrintWriter writer = new PrintWriter("File.txt", "UTF-8");
-		writer.println("The first line");
-		writer.println("The second line");
-		writer.close();
+		PrintWriter writer;
+		try {
+			//writer = new PrintWriter("C:\\Users\\johnsond\\Documents\\NJIT Classes\\CS 490 (Design in Software Engineer)\\cs490\\Text.txt", "UTF-8");
+			writer = new PrintWriter("/afs/cad/u/d/j/dj65/public_html/cs490/output/NewText.txt", "UTF-8");
+			writer.println(args[0]); 
+			writer.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace(); 
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
     }
 }  
