1ddfe8a4715112280bbb8e80dc9127ba2ed290bb
Desmond Johnson <dj65@njit.edu>
2013-10-12 18:33:15 -0400

    execute.php is working well now

--- execute.php
+++ execute.php
@@ -1,16 +1,54 @@
 <?php
-echo '<pre>';
-$newString =  shell_exec('java Compiler "
-String newString = \"someText\";
+
+//Sample code that is not being used
+$code = '
+String newString = "someText";
 System.out.println(newString);
-"
-');
- 
-echo $newString;
-$lines = file('http://web.njit.edu/~dj65/cs490/output/sourceCode.java');
-foreach ($lines as $line) {
-    echo htmlspecialchars($line) . "<br/>";
+';
+
+
+
+
+if(isset($_POST['code'])){
+	echo '<form action="" method="post">
+	<textarea name="code" rows="4" cols="50" autofocus>
+	'.$_POST['code'].'
+	</textarea>
+	<br>
+	<input style="float:left" type="submit" value="Run Code">
+	</form>';
+	echo "<br><br>";
+	echo compiler($_POST['code']);
+
+	//$file = 'http://web.njit.edu/~dj65/cs490/output/sourceCode.java';
+	//readLocalFile($file);
 }
-echo '</pre>';
+else{
+	echo '<form action="" method="post">
+	<textarea name="code" rows="4" cols="50">
+	Enter Code Here
+	</textarea>
+	<br>
+	<input style="float:left" type="submit" value="Run Code">
+	</form>';
+}
+
+
+//Compiles the code passed to it. If it has an error message will be returned, 
+//otherwise the compile program will be ran and the results will be returned
+function compiler($someCode){
+	return '<pre>'.shell_exec('java Compiler '.'\''.$someCode.'\'').'</pre>';
+}
+
+//Reads files directly from the server. Outputs there content to the screen
+function readLocalFile($filePath){ 
+	$lines = file($filePath);
+	echo '<pre>';
+	foreach ($lines as $line) {
+		echo htmlspecialchars($line) . "<br/>";
+	}
+	echo '</pre>';
+}
 
 ?>
+
