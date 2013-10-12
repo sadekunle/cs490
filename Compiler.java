4b80733c70eb550fe4e7007e6b429a55c44d3161
Desmond Johnson <dj65@njit.edu>
2013-10-12 14:06:21 -0400

    Compiler.java and execute.php are in a state where they are good for testing output error messages an just output in general

--- Compiler.java
+++ Compiler.java
@@ -1,10 +1,16 @@
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.io.PrintWriter;
 
+import org.omg.CORBA.portable.InputStream;
+
 public class Compiler {
     public static void main(String[] args) {
+		String returnMessage = "";
 		PrintWriter writer;
 		try {
 			//writer = new PrintWriter("C:\\Users\\johnsond\\Documents\\NJIT Classes\\CS 490 (Design in Software Engineer)\\cs490\\Text.txt", "UTF-8");
@@ -19,9 +25,26 @@ public class Compiler {
 			Runtime rt = Runtime.getRuntime();
 			try {
 				Process pr = rt.exec("javac /afs/cad/u/d/j/dj65/public_html/cs490/output/sourceCode.java");
+				String s = null;
+		        BufferedReader stdInput = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+		        BufferedReader stdError = new BufferedReader(new InputStreamReader(pr.getErrorStream()));
+		           returnMessage = "Here is the standard output of the command:\n\n";
+		           while ((s = stdInput.readLine()) != null) {
+		        	   returnMessage += s+"\n";
+		           }
+		           returnMessage += "\nHere is the standard error of the command (if any):\n\n";
+		           while ((s = stdError.readLine()) != null) {
+		        	   returnMessage += s+"\n";
+		           }
 			} catch (IOException e) {
+			//} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+				String error = e.getMessage();
+				returnMessage = "Get some text";
+				writer = new PrintWriter("/afs/cad/u/d/j/dj65/public_html/cs490/output/error.txt", "UTF-8");
+				writer.println(error);
+				writer.close();
 			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -30,6 +53,6 @@ public class Compiler {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		System.out.println(returnMessage);
     }
 }  
--- execute.php
+++ execute.php
@@ -1,23 +1,15 @@
 <?php
-echo '<pre>';
 
-// Outputs all the result of shellcommand "ls", and returns
-// the last output line into $last_line. Stores the return value
-// of the shell command in $retval.
-//$newString =  exec('java Compiler '.'First Line\nSecond Line');
 $newString =  shell_exec('java Compiler "
-First Line Second Line
-The Next Line
-The fourth Line
+String newString = \"someText\";
+System.out.println(newString);
 "
 ');
  
 echo $newString;
+$lines = file('http://web.njit.edu/~dj65/cs490/output/sourceCode.java');
+foreach ($lines as $line) {
+    echo htmlspecialchars($line) . "<br/>";
+}
 
-/*$last_line = system('ls --help', $retval);*/
-// Printing additional info
-/*echo '
-</pre>
-<hr />Last line of the output: ' . $last_line . '
-<hr />Return value: ' . $retval;*/
 ?>
